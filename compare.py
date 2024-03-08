42ab18355b0d2161a3efdc340b5aaf859860c854
R3tr0 <73380860+Francesco-genovese@users.noreply.github.com>
2024-03-08 21:12:53 +0100

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,9 @@
+# Ottieni le due stringhe dall'utente
+stringa1 = input("Inserisci la prima stringa: ")
+stringa2 = input("Inserisci la seconda stringa: ")
+
+# Confronta le stringhe
+if stringa1 == stringa2:
+    print("Le stringhe sono uguali.")
+else:
+    print("Le stringhe sono diverse.")
